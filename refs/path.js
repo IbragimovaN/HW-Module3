3dfec0586ce43c39b8fc6a142ea550444d36f2a3
Anastasiya <anastasiya.ibr@mail.ru>
2024-03-21 15:57:24 +0300

    код из урока4.работа с es модулями

---
+++ refs/path.js
@@ -0,0 +1,8 @@
+const path = require("path");
+
+// console.log(path.dirname(__dirname));
+// console.log(path.basename(__dirname));
+// console.log(path.extname(__dirname));
+// console.log(path.parse(__filename));
+// console.log(path.resolve(__dirname, "..", "./modules", "./app.js"));
+console.log(path.join(__dirname, "..", "./modules", "./app.js"));
